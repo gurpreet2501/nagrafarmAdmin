bf3eafa8636354fabc36eab8534ee76dbaf3c42e
unknown <gurpreet2501@gmail.com>
2017-02-25 18:36:59 +0530

    all files deldeted

--- application/third_party/lako/config/lako_excepton.php
+++
@@ -1,5 +0,0 @@
-<?php
-lako::get('config')->add_config('lako_exception',array(
-  'write_to_file' => false,
-  'file_name'     => ''
-));
--- application/views/admin/crud.php
+++
@@ -1,9 +0,0 @@
-<div id="content">
-	<div id="content-header">
-		<h1><?= $heading ?></h1>
-	</div> <!-- #content-header -->	
-
-	<div id="content-container">
-      <?= $output ?>
-	</div> <!-- #content -->
-</div>
